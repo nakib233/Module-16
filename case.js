65e978ae073be36e383d7d36de860102fcd9cd53
nakib233 <nakibulhasansadik23@gmail.com>
2025-08-01 02:29:45 +0600

    learning the string and object in JS

---
+++ case.js
@@ -0,0 +1,22 @@
+// const subject = "Chemistry";
+// const book = "chemistry";
+
+// if (subject.toLowerCase() === book.toLowerCase()) {
+//   console.log("I am reading book aibar porikkhai Fatai Felmu");
+// } else {
+//   console.log("Hudai prista ultacci vat khamu.");
+// }
+
+// const school = "Bogura Zilla School";
+// console.log(school);
+// console.log(school.toLocaleLowerCase());
+// console.log(school.toUpperCase());
+
+const drink = " water";
+const liquid = "water ";
+
+if (drink.trim() === liquid.trim()) {
+  console.log("Pani er opr nam life.");
+} else {
+  console.log("Somudre pani ase khaite pari na.");
+}
