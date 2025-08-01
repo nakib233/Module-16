11698843b4b77df97195560ce78ef40b284b49b3
nakib233 <nakibulhasansadik23@gmail.com>
2025-08-01 15:57:38 +0600

    added some important code

---
+++ object.js
@@ -0,0 +1,18 @@
+// Object
+const bottle = {
+  brand: "Apple",
+  price: 45,
+  color: "white",
+  isClean: false,
+};
+
+const subject = {
+  name: "biology",
+  teacher: "Rasheda Mam",
+  examDate: "30 Feb",
+  chapters: ["first", "second", "third"],
+  exams: {
+    name: "Final Exam",
+    marks: 200,
+  },
+};
